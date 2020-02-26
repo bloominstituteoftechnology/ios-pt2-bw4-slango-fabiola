3e3c0f7e22e40dc0c048a73f11a5f342aa4abbde
Fabiola <saga.fabiola@gmail.com>
2020-02-25 21:29:41 -0600

    Added Xcode Project
    Closes #1

---
+++ Slango/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Slango
+//
+//  Created by Fabiola S on 2/25/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Slango/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Slango
+//
+//  Created by Fabiola S on 2/25/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
