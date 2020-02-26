32ac4e50fb2c75250b044feefefb6e08f6c18692
Fabiola <saga.fabiola@gmail.com>
2020-02-25 21:29:41 -0600

    Added Xcode Project
    Closes #1

---
+++ Slango/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Slango
+//
+//  Created by Fabiola S on 2/25/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Slango/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Slango
+//
+//  Created by Fabiola S on 2/25/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
