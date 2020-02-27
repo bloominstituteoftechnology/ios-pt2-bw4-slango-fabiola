8ea2630a93ebc9ab69494e8101b89507d50249c9
Fabiola Saldivar <saga.fabiola@gmail.com>
2020-02-27 09:46:56 -0600

    Organized and created main files
    Closes #5

---
+++ Slango/Slango/Word.swift
@@ -0,0 +1,9 @@
+//
+//  Word.swift
+//  Slango
+//
+//  Created by Fabiola S on 2/27/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+import Foundation
---
+++ Slango/Slango/WordDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WordDetailViewController.swift
+//  Slango
+//
+//  Created by Fabiola S on 2/27/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+import UIKit
+
+class WordDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
