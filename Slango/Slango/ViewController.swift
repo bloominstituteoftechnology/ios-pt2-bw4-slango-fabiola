2a7f4da7e83ca149f3320a63797317dab882aa0d
Fabiola Saldivar <saga.fabiola@gmail.com>
2020-02-27 08:33:35 -0600

    Added new Xcode project
    Closes #6

---
+++ Slango/Slango/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slango
+//
+//  Created by Fabiola S on 2/27/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
