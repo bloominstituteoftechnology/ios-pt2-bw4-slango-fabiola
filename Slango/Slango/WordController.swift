1510e1a2e04cdc7005ede19b5844c53d3519be8a
Fabiola Saldivar <saga.fabiola@gmail.com>
2020-02-27 09:46:56 -0600

    Organized and created main files
    Closes #5

---
+++ Slango/Slango/WordController.swift
@@ -0,0 +1,9 @@
+//
+//  WordController.swift
+//  Slango
+//
+//  Created by Fabiola S on 2/27/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+import Foundation
---
+++ Slango/Slango/WordTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WordTableViewCell.swift
+//  Slango
+//
+//  Created by Fabiola S on 2/27/20.
+//  Copyright © 2020 Fabiola Saga. All rights reserved.
+//
+
+import UIKit
+
+class WordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
