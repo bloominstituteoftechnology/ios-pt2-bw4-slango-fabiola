b68f396d2c5fa50dbd393dd149cac11071e56b06
Fabiola <saga.fabiola@gmail.com>
2020-02-28 12:30:26 -0600

    Updates to ViewController background
    Added a UIView to set background gradient color on HomeVC and WordDetailVC, adjusted UI settings for Cards, Cells and Buttons.

--- Slango/Slango/ButtonShadow.swift
+++ Slango/Slango/ButtonShadow.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class ButtonShadow: UIButton {
+    
+    override func awakeFromNib() {
+        layer.shadowPath = CGPath(rect: layer.bounds, transform:  nil)
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 15
+        
+        layer.cornerRadius = 15
+    }
+}
--- Slango/Slango/CellView.swift
+++ Slango/Slango/CellView.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+class CellView: UIView {
+    
+    override func awakeFromNib() {
+        layer.shadowPath = CGPath(rect: layer.bounds, transform:  nil)
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 2, height: 1)
+        layer.shadowOpacity = 0.4
+        layer.shadowRadius = 15
+        
+        layer.cornerRadius = 15
+    }
+}
+
