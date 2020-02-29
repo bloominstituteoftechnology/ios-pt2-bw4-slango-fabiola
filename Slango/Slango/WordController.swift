240d09a194048b8f70205a36c5ee90e09b28cdc9
Fabiola <saga.fabiola@gmail.com>
2020-02-29 10:39:44 -0600

    Implemented WordController
    Implemented WordController to fetch from words.json file in project
    
    Closes #9

--- Slango/Slango/WordController.swift
+++ Slango/Slango/WordController.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+class WordController {
+    
+    // MARK: Properties
+    private var persistentFileURL: URL? {
+        guard let filePath = Bundle.main.path(forResource: "words", ofType: "json") else { return nil }
+        return URL(fileURLWithPath: filePath)
+    }
+    
+    // MARK: Functions
+    func loadFromPersistentStore(completion: @escaping ([Word]?, Error?) -> Void) {
+        let bgQueue = DispatchQueue(label: "wordQueue", attributes: .concurrent)
+        
+        bgQueue.async {
+            let fm = FileManager.default
+            guard let url = self.persistentFileURL, fm.fileExists(atPath: url.path) else {
+                completion(nil, nil)
+                return
+            }
+            
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let words = try decoder.decode([Word].self, from: data)
+                completion(words, nil)
+            } catch {
+                print("Error loading student data: \(error)")
+                completion(nil, error)
+            }
+        }
+    }
+}
