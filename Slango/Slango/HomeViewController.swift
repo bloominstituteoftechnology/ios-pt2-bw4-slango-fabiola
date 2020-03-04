df1ef47c7734750cd8ba5bf0e493e9cef62bd0c4
Fabiola <saga.fabiola@gmail.com>
2020-03-03 20:53:22 -0600

    Connected TableViewCell to WordDetailViewController

--- Slango/Slango/HomeViewController.swift
+++ Slango/Slango/HomeViewController.swift
@@ -48,15 +48,18 @@ class HomeViewController: UIViewController {
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
     }
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "WordDetailSegue" {
+            if let wordDetailVC = segue.destination as? WordDetailViewController, let indexPath = tableView.indexPathForSelectedRow {
+                wordDetailVC.wordController = wordController
+                wordDetailVC.word = words[indexPath.row]
+            }
+        }
     }
-    */
+    
 }
 
 extension HomeViewController: UITableViewDataSource {
