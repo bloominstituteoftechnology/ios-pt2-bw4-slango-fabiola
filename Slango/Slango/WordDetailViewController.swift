56c1ba74a90551f4e8dd879ca2d9473cc052c30c
Fabiola <saga.fabiola@gmail.com>
2020-03-03 20:53:22 -0600

    Connected TableViewCell to WordDetailViewController

--- Slango/Slango/WordDetailViewController.swift
+++ Slango/Slango/WordDetailViewController.swift
@@ -13,11 +13,17 @@ class WordDetailViewController: UIViewController {
     // MARK: Outlets
     @IBOutlet weak var backgroundGradient: UIView!
     
+    var wordController: WordController?
+    var word: Word? {
+        didSet {
+            updateViews()
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setGradientBackgroundColor()
-        // Do any additional setup after loading the view.
     }
     
     // MARK: - UI
@@ -35,9 +41,10 @@ class WordDetailViewController: UIViewController {
 
     }
     
-    @IBAction func backToHome(_ sender: UIButton) {
-       
+    private func updateViews() {
+        
     }
+    
     /*
     // MARK: - Navigation
 
